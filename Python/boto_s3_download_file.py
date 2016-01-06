568c4e944d01b02f83888f9f7ede684d85743106
Argishti Rostamian <argishti.rostamian@gmail.com>
2016-01-06 12:45:26 -0800

    Boto s3 file download.

---
+++ Python/boto_s3_download_file.py
@@ -0,0 +1,15 @@
+# Download a single file from S3.
+# Example Usage: python boto_s3_download_file.py my_s3_bucket example_file.zip
+
+import sys
+from boto.s3.connection import S3Connection
+from boto.s3.key import Key
+
+s3_bucket = sys.argv[1]
+filename = sys.argv[2]
+
+conn = S3Connection()
+bucket = conn.get_bucket(s3_bucket)
+k = Key(bucket)
+k.key = filename
+k.get_contents_to_filename(filename)
